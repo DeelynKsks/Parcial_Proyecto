528d1b7aa0591ec88994d88b576fe44b5ba63d01
DeelynKsks <zaikeny77@gmail.com>
2022-10-17 13:54:23 -0300

    Path innecesario borrado

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const path = require ("path")
+require ("dotenv").config()
 const express = require ("express")
 const cors = require ("cors")
 const morgan = require ("morgan")
 
 const database = require("./connection")
-require ("dotenv").config()
 
 const app = express()
 database()
 
 const port = process.env.PORT
 
-app.listen(port, ()=>{
-    console.log(`Servidor corriendo en: http://localhost:${port}`)
-})
-
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'src/public')));
-
 app.use(require("./src/routes/user.routes"))
 app.use(require("./src/routes/task.routes"))
 app.use(require("./src/routes/auth.routes"))
+
+app.listen(port, ()=>{
+    console.log(`Servidor corriendo en: http://localhost:${port}`)
+})
